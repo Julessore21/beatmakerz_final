7e2a3f9bd3e736fa281bf00dd5b1bfb74ab19056
Jules Sore-Larregain <Jules.SORE-LARREGAIN@cea.fr>
2025-12-03 11:17:59 +0100

    UPDATE - Dernieres midifications avant déploiement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import { FaInstagram, FaTiktok, FaYoutube, FaXTwitter } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full mt-16 sm:mt-20 text-white/90 border-t border-white/10 bg-transparent">
-      <div className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8 pt-12 pb-14">
+    <footer className="w-full mt-16 sm:mt-20 text-white/90 border-t border-white/10 bg-gradient-to-r from-[#080810] via-[#0a0a12] to-[#080810]">
+      <div className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-10 pt-12 pb-14">
         {/* Top brand row */}
         <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
           <div className="min-w-0">
